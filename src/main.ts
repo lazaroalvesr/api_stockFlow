9e35d34134f53f819f6dfd51dd853cbec0a46f79
lazaro <lazaroalves12355@gmail.com>
2024-08-01 14:58:38 -0300

    999

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "http://localhost:3000"
+    origin: "http://localhost:3000",
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+    credentials: true
   });
 
   const reflector = app.get(Reflector);
