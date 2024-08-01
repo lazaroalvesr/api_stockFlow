0fc8c8bd04b7b45824ae19d384cb22fd58ce57c6
lazaro <lazaroalves12355@gmail.com>
2024-08-01 10:46:18 -0300

    fe

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors()
+  app.enableCors({
+    origin: 'http://localhost:3000/'
+  })
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
   await app.listen(3001);
