0b0a96c983e4b298fa12c6c4a3300d3257f8bb60
lazaro <lazaroalves12355@gmail.com>
2024-08-01 12:51:17 -0300

    ajuste main

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Permite a origem do frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
   await app.listen(3000);
