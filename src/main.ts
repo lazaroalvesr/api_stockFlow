aefcfaf1bdc5b078bfa31dd357e95d6f4f118133
lazaro <lazaroalves12355@gmail.com>
2024-08-01 14:35:13 -0300

    teste44

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 
-  app.enableCors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  });
+  app.enableCors();
 
   await app.listen(3001);
 }
