2bb131c553395892b0b6d8561f233d7880d883ba
lazaro <lazaroalves12355@gmail.com>
2024-08-01 14:31:52 -0300

    teste44

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
