7eb4923c30281fe3b1faf5e8c3b7649105c42973
lazaro <lazaroalves12355@gmail.com>
2024-08-01 14:45:08 -0300

    dede

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { Reflector } from '@nestjs/core';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
   app.enableCors();
 
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
   await app.listen(3001);
 }
 bootstrap();
