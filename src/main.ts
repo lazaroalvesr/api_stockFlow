00a6c1ed8eb273bba89bd7c91ff245c6bbbb8e03
lazaro <lazaroalves12355@gmail.com>
2024-08-01 14:20:07 -0300

    de999

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { Reflector } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-  const frontendUrl = configService.get<string>('FRONTEND_URL');
 
   app.enableCors({
-    origin: frontendUrl,
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: frontendUrl,
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   });
 
